a85f233874a75184a5818fa94cbf6c216758a728
Daniel Briggs <dan.briggs13@gmail.com>
2019-06-28 01:21:27 -0400

    improved UI, added fixed income/expense functionality

--- src/app.py
+++ src/app.py
@@ -5,48 +5,160 @@ from decimal import Decimal
 import sqlite3
 import sys
 import json
+import datetime as dt
 
 app = Flask(__name__)
 
-def init_tables(db_name):
+db_name = 'budget_manager'
+currentDate = dt.datetime.today()
+availableYears = []
+for i in range(currentDate.year - 5, currentDate.year + 1): #allow user to view and upload budgets from last 5 years
+	availableYears.append(i)
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+def getNetIncome(variableExpenses, fixedExpenses, fixedIncome):
+	netIncome = round(Decimal(0),2)
+	for value in variableExpenses:
+		netIncome -= round(Decimal(value),2)
+	for value in fixedExpenses:
+		netIncome -= round(Decimal(value),2)
+	for value in fixedIncome:
+		netIncome += round(Decimal(value),2)
+	return netIncome
+
+def getFixedPortion():
+	fixedExpenses = {}
+	fixedIncome = {}
+
+	try:
+		f = open("fixedExpenses.txt", "r")
+		lines = f.readlines()
+		for expense in lines:
+			name,amount = expense.split(",")
+			if name in fixedExpenses.keys():
+				fixedExpenses[name] += round(Decimal(amount),2)
+			else:
+				fixedExpenses[name] = round(Decimal(amount),2)
+		f.close()
+	except FileNotFoundError:
+		print("fixedExpenses.txt not found in directory", file=sys.stderr)
+
+	try:
+		f2 = open("fixedIncome.txt", "r")
+		lines = f2.readlines()
+		for income in lines:
+			name,amount = income.split(",")
+			if name in fixedIncome.keys():
+				fixedIncome[name] += round(Decimal(amount),2)
+			else:
+				fixedIncome[name] = round(Decimal(amount),2)
+		f2.close()
+	except FileNotFoundError:
+		print("fixedIncome.txt not found in directory", file=sys.stderr)
+
+	return (fixedExpenses, fixedIncome)
+
+def init_tables():
 	conn = sqlite3.connect(db_name)
-	c = conn.cursor
-	c.execute("""CREATE TABLE IF NOT EXISTS monthlyVariableExpenses(startingDate DATE PRIMARY KEY,spendingCategory VARCHAR(32), spendingAmount FLOAT(2));
-				 CREATE TABLE IF NOT EXISTS monthlyVariableIncome(startingDate DATE PRIMARY KEY, incomeCategory VARCHAR(32), incomeAmount FLOAT(2));
-				 CREATE TABLE IF NOT EXISTS fixedExpenses(expenseName VARCHAR(32), spendingCategory VARCHAR(32), spendingAmount FLOAT(2));
-				 CREATE TABLE IF NOT EXISTS fixedIncome(incomeName VARCHAR(32), incomeCategory VARCHAR(32), incomeAmount FLOAT(2));
-				 CREATE TABLE IF NOT EXISTS categoryAdjustments(transactionRegex VARCHAR(32), newCategory VARCHAR(32));""")
+	c = conn.cursor()
+	c.execute("""CREATE TABLE IF NOT EXISTS monthlyVariableExpenses(startingMonth VARCHAR(16), startingYear VARCHAR(4),spendingCategory VARCHAR(32),
+		spendingAmount FLOAT(2))""")
+	conn.commit()
 	conn.close()
+
+@app.route('/deleteBudget', methods=['GET'])
+def deleteBudget():
+	month = request.args.get('month')
+	month = '0' + month[-1] #ensures there is a leading 0 for single digit months
+	year = request.args.get('year')
+
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+
+	c.execute("DELETE FROM monthlyVariableExpenses WHERE startingMonth = ? AND startingYear = ?", [month,year])
+	conn.commit()
+	conn.close()
+	
+	return render_template('index.html',month=int(month), year=int(year), months=months, years = availableYears)
+
+@app.route('/newBudget', methods=['POST'])
+def newBudget():
+	transactionFile = request.files['transactionFile']
+	text = str(transactionFile.read())
+	textNoNewlines = text.replace("\\n",",");
+	splits = textNoNewlines.split(',')
+
+	categorySums = {}
+	categoryKeys = []
+	categoryValues = []
+
+	month,day,year = splits[10].split('/') #get a date that the budget includes to use in database
+
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	c.execute("SELECT startingMonth, startingYEAR FROM monthlyVariableExpenses WHERE startingMonth = ? AND startingYEAR = ?", [month,year])
+	results = c.fetchall()
+
+	if len(results) != 0:
+		return render_template('index.html', month=currentDate.month, year = currentDate.year, months = months, years = availableYears, error=True)
+	
+	#8 header columns so start on 8 to skip headers, but each row has 7 columns since only one of debit/credit is filled 
+	for i in range(9,len(splits),8):
+		if splits[i+4] == "Payment": #represents credit card payment, so skip it
+			continue
+		if splits[i+4] in categorySums.keys():
+			categorySums[splits[i+4]] += round(Decimal(splits[i+5]),2)
+		else:
+			categoryKeys.append(splits[i+4])
+			categorySums[splits[i+4]] = round(Decimal(splits[i+5]),2)
+
+	for k in categoryKeys:
+		categoryValues.append(float(categorySums[k]))
+
+	for k in categorySums.keys():
+		c.execute("INSERT INTO monthlyVariableExpenses VALUES(?,?,?,?)", [month, year, str(k), str(categorySums[k])])
+		conn.commit()
+	conn.close()
+
+	fixedExpenses, fixedIncome = getFixedPortion()
+	netIncome = getNetIncome(categoryValues, fixedExpenses.values(), fixedIncome.values())
+
+
+	return render_template('index.html',data=categorySums, keys = categoryKeys, values = categoryValues, month=int(month),
+ 	  year=int(year), months=months, years = availableYears, fixedIncome = fixedIncome, fixedExpenses = fixedExpenses, netIncome = netIncome)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	if request.method == 'POST': #triggers once user uploads transaction data
-		transactionFile = request.files['transactionFile']
-		text = str(transactionFile.read())
-		textNoNewlines = text.replace("\\n",",");
-		splits = textNoNewlines.split(',')
-		categorySums = {}
-		categoryKeys = []
-		categoryValues = []
-		#8 header columns so start on 8 to skip headers, but each row either has 7 or 8 
-		#columns since credit/debit could be omitted (if credit is filled) or left blank (if debit is filled) 
-		for i in range(9,len(splits),8):
-			advance = 1
-			if splits[i+4] == "Payment": #amount is in last column of table (debit)
-				advance = 2
+	init_tables()
+	
+	if request.method == 'POST': #triggers once user selects month / year to view budget for
+		requestedMonth = request.form['month']
+		requestedMonth = '0' + requestedMonth[-1] #ensures there is a leading 0 for single digit months
+		requestedYear = request.form['year']
 
-			if splits[i+4] in categorySums.keys():
-				categorySums[splits[i+4]] += round(Decimal(splits[i+4+advance]),2)
-			else:
-				categoryKeys.append(splits[i+4])
-				categorySums[splits[i+4]] = round(Decimal(splits[i+4+advance]),2)
+		conn = sqlite3.connect(db_name)
+		c = conn.cursor()
+		c.execute("SELECT spendingCategory, spendingAmount FROM monthlyVariableExpenses WHERE startingMonth = ? AND startingYear = ?",
+		 [requestedMonth,requestedYear])
+		results = c.fetchall()
 
-			if advance == 2: #advances counter 1 more since there is an extra column since debit was left blank
-				i += 1
+		if len(results) == 0: #no existing budget for selected month, just display generic page
+			return render_template('index.html', month=int(requestedMonth), year = int(requestedYear), months = months, years = availableYears,
+				noBudgetFound = True)
+		else: #existing budget found, display transaction table and chart
+			categorySums = {}
+			categoryKeys = []
+			categoryValues = []
+			for result in results:
+				categorySums[result[0]] = result[1]
+				categoryKeys.append(result[0])
+				categoryValues.append(result[1])
 
-		for k in categoryKeys:
-			categoryValues.append(float(categorySums[k]))
+			fixedExpenses, fixedIncome = getFixedPortion()
+			netIncome = getNetIncome(categoryValues, fixedExpenses.values(), fixedIncome.values())
 
-		return render_template('index.html',data=categorySums, keys = categoryKeys, values = categoryValues)
+			return render_template('index.html',data=categorySums, keys = categoryKeys, values = categoryValues, month=int(requestedMonth),
+ 	  		  year=int(requestedYear), months=months, years = availableYears, fixedExpenses = fixedExpenses, fixedIncome = fixedIncome,
+ 	  		  netIncome = netIncome)
 	else: #triggers on initial loading of page
-		return render_template('index.html', error=False)
+		return render_template('index.html', month=currentDate.month, year = currentDate.year, months = months, years = availableYears)
